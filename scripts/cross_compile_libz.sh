d64790ace0db49df2606728bd4e2573a10bb14d3
t3sserakt <t3ss@posteo.de>
2024-06-15 12:18:24 +0200

    Make libz cross compile a shared library.

--- scripts/cross_compile_libz.sh
+++ scripts/cross_compile_libz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clean -f
-./configure
+/configure --prefix=/home/user/AndroidStudioProjects/GNUnet/distribution/zlib/lib/arm64-v8a    --shared --libdir=/home/user/AndroidStudioProjects/GNUnet/distribution/zlib/lib/arm64-v8a
 make
-cp ./libz.a ~/AndroidStudioProjects/GNUnet/distribution/libz/lib/arm64-v8a/
+cp cp libz.so.1.2.11 ~/AndroidStudioProjects/GNUnet/distribution/libz/lib/arm64-v8a/libz.so
 cp zlib.h ~/AndroidStudioProjects/GNUnet/distribution/libz/lib/arm64-v8a/include/
